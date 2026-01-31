e1895b102ec44c6039061fb22325c2a6cb112599
mhbb8897 <mahbub.ahwani89@gmail.com>
2026-01-31 13:55:44 +0700

    commit before exam

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Login;
 use App\Models\EncryptController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/get-public-key', [EncryptController::class, 'getPublicKey']);
-Route::post('/loginuser', [AuthController::class, 'login']);
+Route::post('/loginuser', [Login::class, 'login']);
 Route::get('/ping', function () {
     return response()->json([
         'status' => 'ok',
